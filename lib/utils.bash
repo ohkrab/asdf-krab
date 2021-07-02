d160099a2414868127127ee62e2d67ecd52b98fa
qbart <wojtowicz.bartlomiej@gmail.com>
2021-07-02 03:54:42 +0200

    Update download url

--- lib/utils.bash
+++ lib/utils.bash
@@ -38,7 +38,9 @@ download_release() {
   version="$1"
   filename="$2"
 
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  local -r platform="$(get_platform)"
+  local -r arch="$(get_arch)"
+  url="$GH_REPO/releases/download/v${version}/krab_${version}_${platform}_${arch}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -66,4 +68,17 @@ install_version() {
     rm -rf "$install_path"
     fail "An error ocurred while installing $TOOL_NAME $version."
   )
+}
+
+get_arch() {
+  local -r machine="$(uname -m)"
+  if [[ $machine == "arm64" ]] || [[ $machine == "aarch64" ]]; then
+    echo "arm64"
+  else
+    echo "amd64"
+  fi
+}
+
+get_platform() {
+  uname | tr '[:upper:]' '[:lower:]'
 }
